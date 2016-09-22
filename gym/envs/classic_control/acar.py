9e39f5d3fdab148dcb1d40e9ad7a96d0cddcc7a9
silvicek <silvicek@gmail.com>
2016-09-22 10:24:58 +0200

    almost correct astar

--- gym/envs/classic_control/acar.py
+++ gym/envs/classic_control/acar.py
@@ -15,6 +15,8 @@ from pymunk.vec2d import Vec2d
 from pymunk.pygame_util import draw
 import os
 
+from pathfinding import Trajectory
+
 
 import logging
 logger = logging.getLogger(__name__)
@@ -135,6 +137,8 @@ class ACar(gym.Env):
         self.observation_space = spaces.Box(low=0, high=100, shape=(self.state_dim,))
 
         self.full_state = np.zeros(self.state_dim)
+        self.counter = 0
+        self.trajectory = Trajectory(self.car.body.position, self.target.body.position, self._is_empty).trajectory
 
     def _step(self, action):
         self.last_position = copy.copy(self.car.body.position)
@@ -159,14 +163,27 @@ class ACar(gym.Env):
         driving_direction = Vec2d(1, 0).rotated(self.car.body.angle)
         self.car.body.velocity = int(100 * go) * driving_direction
 
+
+
         # Update the screen and stuff.
         self.space.step(1. / 10)
 
         self.screen.fill(THECOLORS["black"])
+
+
         draw(self.screen, self.space)
+        for point in self.trajectory:
+            xc, yc = tup_to_int(point)
+            self.screen.set_at((xc, height-yc), (255, 255, 255))
+
+        xc, yc = tup_to_int(self.car.body.position)
+
         if self.draw_screen:
             pygame.display.flip()
         self.clock.tick()
+
+        import time
+        # time.sleep(10)
 
         # Get the current location and the readings there.
         x, y = self.car.body.position
@@ -222,13 +239,23 @@ class ACar(gym.Env):
                     break
             placed.append(shape)
 
-        # self.car.body.position = 100, 100
-        # self.car.body.velocity = Vec2d(0, 0)
-        # self.car.body.angle = 0
-        #
-        # self.target.body.position = 500, 500
-        # self.target.body.velocity = Vec2d(0, 0)
-        # self.target.angle = 0
+        self.screen.fill(THECOLORS["black"])
+        draw(self.screen, self.space)
+        self.trajectory = Trajectory(self.car.body.position, self.target.body.position, self._is_empty).trajectory
+        for point in self.trajectory:
+            xc, yc = tup_to_int(point)
+            self.screen.set_at((xc, yc), (255, 255, 255))
+
+        for x in [self.car, self.target] + self.obstacles:
+            pygame.draw.circle(self.screen, (255, 255, 255), tup_to_int(x.body.position), 10)
+
+        if self.draw_screen:
+            pygame.display.flip()
+
+        import time
+        time.sleep(5)
+
+        print 'counter', self.counter
 
         return self._step(None)[0]
 
@@ -327,8 +354,7 @@ class ACar(gym.Env):
                     or rotated_p[0] >= width or rotated_p[1] >= height:
                 return i  # Sensor is off the screen.
             else:
-                obs = self.screen.get_at(rotated_p)
-                if self._is_empty(obs) != 0:
+                if not self._is_empty(rotated_p):
                     return i
 
             if self.show_sensors:
@@ -358,12 +384,14 @@ class ACar(gym.Env):
         new_y = height - (y_change + y_1)
         return int(new_x), int(new_y)
 
-    def _is_empty(self, reading):  # TODO: fix this stupid color-check
-        if reading == THECOLORS['black'] or reading == THECOLORS['green'] \
-                or reading == THECOLORS['blue'] or reading == THECOLORS['orange']:
-            return 0
-        else:
-            return 1
+    def _is_empty(self, p):  # TODO: fix this stupid color-check
+        self.counter += 1
+        try:
+            reading = self.screen.get_at(tup_to_int(p))
+        except IndexError:
+            return False
+        return reading == THECOLORS['black'] or reading == THECOLORS['green'] \
+                or reading == THECOLORS['blue'] or reading == THECOLORS['orange']
 
 
 def bin_from_int(a, len):
@@ -376,22 +404,22 @@ def bin_from_int(a, len):
 def norm_pi(angle):
     return angle - 2*np.pi*np.floor((angle + np.pi) / (2*np.pi))
 
+class Args:
+    def __init__(self):
+        self.mode = 'play'
+        self.memory_steps = 0
+
+
+def tup_to_int(x):
+    return (int(x[0]), int(x[1]))
+
+
 if __name__=="__main__":
-    from pyglet.window import key
+    import time
+    np.random.seed(1337)
     a = np.array( [1.0, 0.0, 0.0] )
-    def key_press(k, mod):
-        global restart
-        if k==0xff0d: restart = True
-        if k==key.LEFT:  a[0] = -1.0
-        if k==key.RIGHT: a[0] = +1.0
-        if k==key.UP:    a[1] = +1.0
-        if k==key.DOWN:  a[2] = +0.8   # set 1.0 for wheels to block to zero rotation
-    def key_release(k, mod):
-        if k==key.LEFT  and a[0]==-1.0: a[0] = 0
-        if k==key.RIGHT and a[0]==+1.0: a[0] = 0
-        if k==key.UP:    a[1] = 0
-        if k==key.DOWN:  a[2] = 0
     env = ACar()
+    env.configure(Args())
     env.render()
     record_video = False
     if record_video:
@@ -402,10 +430,11 @@ if __name__=="__main__":
         steps = 0
         restart = False
         while True:
+            a = env.action_space.sample()
             s, r, done, info = env.step(a)
+            time.sleep(0.01)
             total_reward += r
             if steps % 200 == 0 or done:
-                print("\naction " + str(["{:+0.2f}".format(x) for x in a]))
                 print("step {} total_reward {:+0.2f}".format(steps, total_reward))
                 #import matplotlib.pyplot as plt
                 #plt.imshow(s)
